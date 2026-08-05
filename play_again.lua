aeb084a20d3a38462a94354cc7eba3ab829c9306
yozba <45725762+yozba@users.noreply.github.com>
2026-08-05 18:03:40 +0900

    Refactor play_again function and update key bindings

--- play_again.lua
+++ play_again.lua
@@ -1,7 +1,10 @@
-mp.add_key_binding("Space", function()
+local function play_again()
     if mp.get_property_native("eof-reached") then
         mp.command("no-osd seek 0 absolute ; set pause no")
     else
         mp.command("cycle pause")
     end
-end)
+end
+
+mp.add_key_binding("Space", play_again)
+mp.add_key_binding("MBTN_RIGHT", play_again)
